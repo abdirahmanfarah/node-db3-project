ffe581c6a5944d6a3d98c484ab9fc6fe327ea279
abdirahmanfarah <abdirahman.z.farah@gmail.com>
2020-01-22 18:54:51 -0800

    MVP

--- server.js
+++ server.js
@@ -5,6 +5,11 @@ const SchemeRouter = require('./schemes/scheme-router.js');
 const server = express();
 
 server.use(express.json());
+
+server.get('/', (req, res) => {
+  res.send( "Server up and running!")
+});
+
 server.use('/api/schemes', SchemeRouter);
 
 module.exports = server;
